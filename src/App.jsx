e48ed12f55ccf9a6430d7d3a053b45ffc391d865
Caio <caiokaea@gmail.com>
2025-10-24 11:19:08 -0300

    feat: product section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // Components
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import Product from "./components/Product";
 
 const App = () => {
   return (
     <div className="bg-black">
       <NavBar />
       <Hero />
+      <Product />
     </div>
   );
 };
---
+++ src/components/three/StudioLights.jsx
@@ -0,0 +1,42 @@
+import { Environment, Lightformer, SpotLight } from "@react-three/drei";
+
+const StudioLights = () => {
+  return (
+    <group>
+      <Environment resolution={256}>
+        <group>
+          <Lightformer
+            form="rect"
+            intensity={15}
+            position={[-10, 10, -10]}
+            scale={10}
+            rotateY={Math.PI / 2}
+          />
+          <Lightformer
+            form="rect"
+            intensity={15}
+            position={[10, 10, 10]}
+            scale={10}
+            rotateY={Math.PI / 2}
+          />
+        </group>
+      </Environment>
+
+      <SpotLight
+        position={[0, 4.6, 1.5]}
+        angle={90}
+        decay={0}
+        intensity={Math.PI * 0.5}
+      />
+
+      <SpotLight
+        position={[0, 2, 4]}
+        angle={30}
+        decay={0}
+        intensity={Math.PI * 0.5}
+      />
+    </group>
+  );
+};
+
+export default StudioLights;

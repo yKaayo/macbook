aeb247931e461248b75e6068b2c45bd1e1017ea0
Caio <caiokaea@gmail.com>
2025-11-01 23:02:20 -0300

    fix: improving performance of graphic section and take out the material desnecessary in 3d

--- src/components/models/LargeMacbook.jsx
+++ src/components/models/LargeMacbook.jsx
@@ -122,7 +122,6 @@ export default function LargeMacbookModel(props) {
       />
       <mesh
         geometry={nodes.Object_123.geometry}
-        material={materials.sfCQkHOWyrsLmor}
         rotation={[Math.PI / 2, 0, 0]}
       >
         <meshBasicMaterial map={texture} />
